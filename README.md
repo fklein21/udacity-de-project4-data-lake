d532d72940e593f3f72065e6e8a4c362e05748e8
Ferdinand Kleinschroth <ferdinand.kleinschroth@gmx.de>
2022-12-26 23:22:03 +0100

    1st submit

--- README.md
+++ README.md
@@ -1,1 +1,105 @@
-# udacity-de-project4-data-lake
+<!-- Improved compatibility of back to top link: See: https://github.com/othneildrew/Best-README-Template/pull/73 -->
+<a name="readme-top"></a>
+<!--
+*** Thanks for checking out the Best-README-Template. If you have a suggestion
+*** that would make this better, please fork the repo and create a pull request
+*** or simply open an issue with the tag "enhancement".
+*** Don't forget to give the project a star!
+*** Thanks again! Now go create something AMAZING! :D
+-->
+
+
+# Project 4: Data Lake
+
+<p style='text-align: right;'> 26/12/22 </p>
+
+### Udacity Bosch AI Talent Accelerator Scholarship
+### Ferdinand Kleinschroth
+(ferdinand.kleinschroth@gmx.de)
+<div align="center"> 
+
+![](images/Cloud_Data_Warehouses-Udacity.png)
+</div>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+The objective of the project was to create an ETL pipeline to extract their data from AWS S3 buckets, process them using Spark to create dimensional tables and move the processed data back to S3 buckets.
+
+The data consists of:
+- a song library in the form of a collection of json files with information about each song and
+- log data about user activity, also in the form of json files.
+
+The processed data was saved in the S3 bucket in the parquet format. It is in a star schema, saved in the following files:
+- `songplays.parquet` (fact table): Contains all entries of the log files which are associated with a song play. The columns are   
+  - `songplay_id`, 
+  - `start_time`, 
+  - `user_id`, 
+  - `level`, 
+  - `song_id`, 
+  - `artist_id`, 
+  - `session_id`, 
+  - `location`, 
+  - `user_agent`
+- `users.parquet` (dimension table): Contains user information.
+  - `user_id`, 
+  - `first_name`, 
+  - `last_name`, 
+  - `gender`, 
+  - `level`
+- `songs.parquet` (dimension table): Contains information about the songs in the song library.
+  - `song_id`, 
+  - `title`, 
+  - `artist_id`, 
+  - `year`, 
+  - `duration`
+- `artists.parquet` (dimension table): Contains information about the artists.
+  - `artist_id`, 
+  - `name`, 
+  - `location`, 
+  - `latitude`, 
+  - `longitude`
+- `time.parquet` (dimension table): Timestamps of each log event split up into hour, day, ...
+  - `start_time`, 
+  - `hour`, 
+  - `day`, 
+  - `week`, 
+  - `month`, 
+  - `year`, 
+  - `weekday`
+
+
+
+## How to run the `etl.py` script
+
+The script needs to be run on a AWS EMR cluster with the configuration as specified in the project instructions on the Udacity website.
+
+The following parameters have to be specified:
+- input_file: The location (s3_bucket) of the input files.
+- output_file: The location (s3_bucket) to which the output should be written.
+
+## Files in the repository
+
+- `etl.py` : Python script that processes the data as described above.
+- `etl.ipynb`: Jupyter notebook to develop the pipeline before putting the logic in the Python script.
+- `README.md` : This readme file
+
+## Discussion of future improvements
+
+In the data processing step no analysis of data skewness was done.
+The data is partitioned in year and month (songplays and time) and year and artist_id (songs), respectively. 
+The data could be skewed in several ways:
+- The user base has grown a lot in the past. There are much more songplay events in more recent years.
+- Some artists may accumulate a large part of the songplays (songs by very famous artists), whereas the majority of artists have only very few songplays.
+- The release date of songs may not be equally distributed of the years.
+
+In a future release this should be taken into account when deciding on a partitioning strategy.
+
+
+
+## Acknowledgments
+
+- The template for the README was taken from [Best-README-Template (GitHub repo)](https://github.com/othneildrew/Best-README-Template).
+- The image at the top of the file was taken from the Udacity website.
---
+++ etl.py
@@ -0,0 +1,199 @@
+# import configparser
+from datetime import datetime
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, col, to_timestamp, concat_ws
+from pyspark.sql.functions import year, month, dayofmonth, dayofweek, hour, weekofyear, date_format
+
+
+# config = configparser.ConfigParser()
+# config.read('dl.cfg')
+
+# os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
+# os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+
+
+def create_spark_session():
+    spark = SparkSession \
+        .builder \
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+        .getOrCreate()
+    return spark
+
+
+def process_song_data(spark, input_data, output_data):
+    '''Processes the song data.
+
+    This function reads the song data (in json format) from 
+    an input S3 bucket, processes it and writes the processed data
+    into an output S3 bucket.
+
+    Two parquet files will be written to the output directory containing 
+    the dimensional data about the songs and the artists.
+
+    Args:
+        spark (SparkSession) : The spark session that executes the computation.
+        input_data (str) : The path to the directory (in a S3 bucket) containing 
+                           the input files.
+        output_data (str) : The path to the directory (in a S3 bucket) where
+                            the processed data should be written.
+    '''
+    # get filepath to song data file
+    song_data = input_data + "/song_data/*/*/*/*"
+    
+    # read song data file
+    df = spark.read.json(song_data).distinct()
+
+    # extract columns to create songs table
+    songs_table = (
+        df
+        .select(
+            "song_id",
+            "title",
+            "artist_id",
+            "year",
+            "duration",
+        )
+        .repartition("year", "artist_id")
+    )
+    
+    # write songs table to parquet files partitioned by year and artist
+    songs_table.write.parquet(output_data + "songs.parquet")
+
+    # extract columns to create artists table
+    artists_table = (
+        df
+        .select(
+            "artist_id",
+            col("artist_name").alias("name"),
+            col("artist_location").alias("location"),
+            col("artist_latitude").alias("latitude"),
+            col("artist_longitude").alias("longitude"),
+        )
+        .distinct()
+        .repartition("artist_id")
+    )
+    
+    # write artists table to parquet files
+    artists_table.write.parquet(output_data + "artists.parquet")
+
+
+def process_log_data(spark, input_data, output_data):
+    '''Processes the log data.
+
+    This function reads the log data (in json format) from 
+    an input S3 bucket, processes it and writes the processed data
+    into an output S3 bucket.
+
+    Three parquet files will be written to the output directory containing 
+        - the dimensional data about the users and the timestamps, and
+        - the fact table of all song play events.
+        
+    Args:
+        spark (SparkSession) : The spark session that executes the computation.
+        input_data (str) : The path to the directory (in a S3 bucket) containing 
+                           the input files.
+        output_data (str) : The path to the directory (in a S3 bucket) where
+                            the processed data should be written.
+    '''
+    # get filepath to log data file
+    log_data = input_data + "log_data/*/*"
+
+    # read log data file
+    df = spark.read.json(log_data)
+    
+    # # filter by actions for song plays
+    df = df.filter(col("page") == "NextSong")
+
+    # extract columns for users table
+    df = (
+        df
+        .withColumnRenamed("userId", "user_id")
+        .withColumnRenamed("firstName", "first_name")
+        .withColumnRenamed("lastName", "last_name")
+    )
+    users_table = (
+        df
+        .select(
+            "user_id",
+            "first_name",
+            "last_name",
+            "gender",
+            "level"
+        )
+        .distinct()
+        .repartition("user_id")
+    )
+
+    # write users table to parquet files
+    users_table.write.parquet(output_data + "users.parquet")
+
+    # create timestamp column from original timestamp column
+    df = df.withColumn("start_time", to_timestamp(col("ts")/1000))
+    
+    # # extract columns to create time table
+    time_table = (
+        df
+        .select("start_time")
+        .distinct()
+        .withColumn("hour", hour("start_time"))
+        .withColumn("day", dayofmonth("start_time"))
+        .withColumn("week", weekofyear("start_time"))
+        .withColumn("year", year("start_time"))
+        .withColumn("weekday", (dayofweek("start_time") - 1) % 7 + 1)
+        .repartition("year", month(col("start_time")))
+    )
+    
+    # write time table to parquet files partitioned by year and month
+    time_table.write.parquet(output_data + "time.parquet")
+
+    # read in song data to use for songplays table
+    song_df = spark.read.parquet(output_data + "songs.parquet")
+
+    # extract columns from joined song and log datasets to create songplays table 
+    songplays_table = (
+        df
+        .join(
+            song_df,
+            [
+                df["song"] == song_df["title"],
+                df["length"] == song_df["duration"]
+            ],
+            "left"
+        )
+        .withColumnRenamed("sessionId", "session_id")
+        .withColumnRenamed("userAgent", "user_agent")
+        .withColumn("songplay_id", concat_ws("-", 
+                                             col("session_id"),
+                                             col("ts").cast("string")))
+        .select(
+            "songplay_id",
+            "start_time",
+            "user_id",
+            "level",
+            "song_id",
+            "artist_id",
+            "session_id",
+            "location",
+            "user_agent"
+        )
+        .repartition(year(col("start_time")), month(col("start_time")))
+    )
+
+    # write songplays table to parquet files partitioned by year and month
+    songplays_table.write.parquet(output_data + "songplays.parquet")
+
+
+def main():
+    spark = create_spark_session()
+    input_data = "s3a://udacity-dend/"
+    output_data = "s3a://spark-project-output-9/"
+    
+    process_song_data(spark, input_data, output_data)    
+    process_log_data(spark, input_data, output_data)
+
+    spark.stop()
+
+
+if __name__ == "__main__":
+    main()
